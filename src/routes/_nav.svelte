4febf04609aafe0a66d2805a4d35226c21d36c7a
SDBagel <sdbagel@gmail.com>
2021-10-22 11:00:37 -0700

    style: Add nav focus style

--- src/routes/_nav.svelte
+++ src/routes/_nav.svelte
@@ -9,7 +9,7 @@
   <a href="">Calendar</a>
   <a href="">Contact</a>
   <a href="">Join NAHS</a>
-  <a href="">Art Show</a>
+  <a class="focus" href="">Art Show</a>
 </nav>
 
 <style lang="scss">
@@ -18,15 +18,20 @@
     align-items: center;
     column-gap: 10px;
     height: 50px;
+    margin: 10px 30px;
   }
 
   a {
-    background-color: #a0a;
+    background-color: #707;
     border-radius: 5px;
     color: #fff;
 
-    padding: 5px 10px;
+    padding: 8px 20px;
 
     text-decoration: none;
+  }
+
+  a.focus {
+    background-color: #d08;
   }
 </style>
