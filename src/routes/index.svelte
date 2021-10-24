f9d14f73442a92ab58e473cd60480e4e61a96c09
SDBagel <sdbagel@gmail.com>
2021-10-24 00:32:01 -0700

    feat: Add landing about, etc. content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,26 @@
 
 <section class="hero">
   <img src="/img/wave.svg" alt="Wave" aria-hidden="true">
-  <div class="hero-content">
+  <div class="hero-content container-wide">
     <h1>LAHS<br>National Art<br>Honors Society</h1>
   </div>
 </section>
-<section>
-
+<div class="hero-spacer" />
+<section class="content container-wide flex-column flex-gap">
+  <div class="flex-smart flex-gap align-center">
+    <figure class="flex">
+      <img src="/img/group.jpg" alt="NAHS Members">
+      <figcaption>2021-2022 Los Altos NAHS Members</figcaption>
+    </figure>
+    <div class="flex">
+      <h2>About Los Altos NAHS</h2>
+      <p>To many, creating art is an important form of expression. However, in Silicon Valley, art is often shadowed by STEM and other academic subjects. Our goal is not only to gain knowledge and create fun projects, but also to make a positive impact in the community through these projects and spread the importance of art! You don't have to be the best artist. We're looking for people who are hardworking and passionate!</p>
+    </div>
+  </div>
+  <div>
+    <h2>Artist Spotlight</h2>
+    <p>The Artist Spotlight will be used to highlight the young artists at LAHS and their art work! It will featured on the NAHS Instagram and Website, daily announcement, and our advisor's Canvas announcements.  The first feature will start on 10/2 and continue biweekly throughout the year.</p>
+  </div>
 </section>
 
 <style lang="scss">
@@ -19,7 +33,7 @@
 
     img {
       position: absolute;
-      top: 100%;
+      top: calc(100% - 5px);
       left: 0;
       right: 0;
     }
@@ -29,8 +43,6 @@
       top: 30%;
       left: 0;
       right: 0;
-      margin: auto;
-      max-width: 1400px;
 
       color: #fff;
       flex: 1 1;
@@ -43,6 +55,34 @@
       user-select: none;
       -moz-user-select: none;
       -webkit-user-select: none;
+    }
+  }
+
+  .hero-spacer {
+    height: 20vw;
+  }
+
+  .content {
+    font-size: 1.4em;
+    line-height: 1.5;
+
+    img {
+      box-shadow: 5px 5px 15px #0003;
+      border-radius: 20px;
+      max-width: 100%;
+    }
+
+    figure {
+      margin: 0;
+    }
+
+    figcaption {
+      font-size: 1rem;
+      opacity: 0.8;
+    }
+
+    h2 {
+      margin-top: 0;
     }
   }
 </style>
