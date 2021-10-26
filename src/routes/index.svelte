9f1023b8b2b6dc502c0dfad26494b85c04c06ce4
SDBagel <sdbagel@gmail.com>
2021-10-26 10:13:46 -0700

    style: Restrict artist spotlight img width

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,7 +23,7 @@
       <h2>Artist Spotlight</h2>
       <p>The Artist Spotlight will be used to highlight the young artists at LAHS and their art work! It will featured on the NAHS Instagram and Website, daily announcement, and our advisor's Canvas announcements.  The first feature will start on 10/2 and continue biweekly throughout the year.</p>  
     </div>
-    <div class="flex-smart flex-gap">
+    <div class="flex-smart flex-gap art-spotlight">
       <span><img class="flex" src="/spotlight/1.png" alt="Artist Spotlight"></span>
       <span><img class="flex" src="/spotlight/2.png" alt="Artist Spotlight"></span>
     </div>
@@ -103,6 +103,11 @@
 
     h2 {
       margin-top: 0;
+    }
+
+    .art-spotlight img {
+      margin-top: 30px;
+      max-width: 350px;
     }
   }
 </style>
