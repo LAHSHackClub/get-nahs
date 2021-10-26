52fc1db1f5738d29b8440d7f5b738b62796e6ed4
SDBagel <sdbagel@gmail.com>
2021-10-25 23:29:31 -0700

    style: Use brand as home link

--- src/routes/_nav.svelte
+++ src/routes/_nav.svelte
@@ -4,12 +4,11 @@
 </script>
 
 <nav class="container-wide">
-  <h2>
+  <a class="brand" href="/"><h2>
     <img src="/img/logo.png" alt="NAHS">
     <span>LAHS x NAHS</span>
-  </h2>
+  </h2></a>
   <span class="spacer"></span>
-  <a href="/">Home</a>
   <a href="">Activities</a>
   <a href="">Calendar</a>
   <a href="">Contact</a>
@@ -41,13 +40,15 @@
   }
 
   a {
+    text-decoration: none;
+  }
+
+  a:not(.brand) {
     background-color: #707;
     border-radius: 5px;
     color: #fff;
 
     padding: 8px 20px;
-
-    text-decoration: none;
   }
 
   a.focus {
