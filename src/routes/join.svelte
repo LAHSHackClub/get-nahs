492c05a15aa9316efca18ac6e863bcc423f4b07f
SDBagel <sdbagel@gmail.com>
2021-11-14 11:57:07 -0800

    Update join page styling

--- src/routes/join.svelte
+++ src/routes/join.svelte
@@ -1,4 +1,10 @@
 
-<section class="container-wide join">
-  <h2>Join NAHS</h2>
-</section>
+<script lang="ts">
+  import Content from '$lib/Content.svelte';
+</script>
+
+<Content>
+  <section class="container-wide join">
+    <h1>Join NAHS</h1>
+  </section>
+</Content>
