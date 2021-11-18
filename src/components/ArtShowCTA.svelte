1abe2a723514e9e8be71a418b6a595bf92b6d4ff
SDBagel <sdbagel@gmail.com>
2021-11-17 18:44:22 -0800

    Abstract AS CTA into component

---
+++ src/components/ArtShowCTA.svelte
@@ -0,0 +1,75 @@
+
+<div class="cta"><a href="/show/0"><span>View Gallery &gt;</span></a></div>
+<div class="cta-mobile"><a href="/show/0">View Gallery &gt;</a></div>
+
+<style lang="scss">
+  .cta-mobile {
+    display: none;
+
+    a {
+      background-image: linear-gradient(45deg, #f09, #c0f, #0af, #0fa);
+      background-repeat: repeat;
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      font-weight: 700;
+      font-size: 2rem;
+    }
+  }
+
+  @media (max-width: 992px) {
+    .cta {
+      display: none !important;
+    }
+    .cta-mobile {
+      display: grid;
+      place-items: center;
+    }
+  }
+
+  .cta, .cta a, .cta a span {
+    display: grid;
+    place-items: center;
+  }
+  .cta {
+    padding: 20px;
+
+    a {
+      text-decoration: none;
+      font-size: 2rem;
+      font-weight: bold;
+      margin-top: 80px;
+      border-radius: 5px;
+      position: relative;
+    }
+    a span {
+      background-color: #000;
+      color: #fff;
+      width: 330px;
+      height: 240px;
+      position: relative;
+      z-index: 2;
+    }
+    a::before, a::after {
+      background-image: linear-gradient(45deg, #f09, #c0f, #0af, #0fa);
+      background-repeat: no-repeat;
+      background-position-x: 300px;
+      background-position-y: 210px;
+      border-radius: 5px;
+      content: "";
+      width: 340px;
+      height: 250px;
+      position: absolute;
+      z-index: 1;
+      transition-duration: 0.6s;
+    }
+    a::before {
+      background-position-x: -300px;
+      background-position-y: -210px;
+    }
+    a:hover::before, a:hover::after {
+      background-position-x: 0px;
+      background-position-y: 0px;
+    }
+  }
+</style>
--- src/routes/show/index.svelte
+++ src/routes/show/index.svelte
@@ -1,5 +1,6 @@
 
 <script lang="ts">
+  import ArtShowCTA from '../../components/ArtShowCTA.svelte';
   import ArtShowThumbnail from '../../components/ArtShowThumbnail.svelte';
   import Content from '../../lib/Content.svelte';
   import { onMount } from 'svelte';
@@ -22,10 +23,7 @@
         <h3 class="styled">Presented by <b>National Art Honors Society</b></h3>
         <p>The 2021 Art Show highlights the talent of LAHS. Students submit artworks from a variety of disciplines, mediums, and class levels. Click or tap on any artwork to enter the gallery view, and scroll to navigate.</p>
       </div>
-      <div class="cta">
-        <a href="/show/0"><span>View Gallery &gt;</span></a>
-      </div>
-      <div class="cta-mobile"><a href="/show/0">View Gallery &gt;</a></div>
+      <ArtShowCTA />
     </div>
     {#each uniqueClasses as c}
       <div class="container-wide">
@@ -79,13 +77,13 @@
       }
     }
 
+    h2 {
+      margin-bottom: 0;
+    }
+
     h3 {
       margin: 0;
       margin-bottom: 50px;
-    }
-
-    h2 {
-      margin-bottom: 0;
     }
 
     p {
@@ -102,73 +100,6 @@
       @media (max-width: 1300px) {
         font-size: 1rem;
       }
-    }
-
-
-    .cta-mobile {
-      display: none;
-    }
-    .cta-mobile a {
-      background-image: linear-gradient(45deg, #f09, #c0f, #0af, #0fa);
-      background-repeat: repeat;
-      background-clip: text;
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-      font-weight: 700;
-      font-size: 2rem;
-    }
-    @media (max-width: 992px) {
-      .cta {
-        display: none !important;
-      }
-      .cta-mobile {
-        display: grid;
-        place-items: center;
-      }
-    }
-    .cta, .cta a, .cta a span {
-      display: grid;
-      place-items: center;
-    }
-    .cta {
-      padding: 20px;
-    }
-    .cta a {
-      text-decoration: none;
-      font-size: 2rem;
-      font-weight: bold;
-      margin-top: 80px;
-      border-radius: 5px;
-      position: relative;
-    }
-    .cta a span {
-      background-color: #000;
-      color: #fff;
-      width: 330px;
-      height: 240px;
-      position: relative;
-      z-index: 2;
-    }
-    .cta a::before, .cta a::after {
-      background-image: linear-gradient(45deg, #f09, #c0f, #0af, #0fa);
-      background-repeat: no-repeat;
-      background-position-x: 300px;
-      background-position-y: 210px;
-      border-radius: 5px;
-      content: "";
-      width: 340px;
-      height: 250px;
-      position: absolute;
-      z-index: 1;
-      transition-duration: 0.6s;
-    }
-    .cta a::before {
-      background-position-x: -300px;
-      background-position-y: -210px;
-    }
-    .cta a:hover::before, .cta a:hover::after {
-      background-position-x: 0px;
-      background-position-y: 0px;
     }
   }
 </style>
