beb3cf667a89c7260f1799d2a00adf37c3d49b47
SDBagel <sdbagel@gmail.com>
2021-11-23 21:27:43 -0800

    Add bylaws and join info

--- src/routes/join.svelte
+++ src/routes/join.svelte
@@ -6,5 +6,128 @@
 <Content>
   <section class="container-wide join">
     <h1>Join NAHS</h1>
+    <p>Joining NAHS is fun and rewarding as you get to use your creativity for various service projects and activities. Look for NAHS on Club Day or email our <a href="/contact">our club advisor</a> for more information about joining.</p>
+    <h2>NAHS Bylaws</h2>
+    <p>To be an LAHS NAHS member, you need to adhere to the following set of bylaws:</p>
+    <ol>
+      <li><p>To remain a member in good standing, a member must complete 10 service hours in the visual arts each semester</p></li>
+      <li><p>Annual dues of $5 will be collected from each member.</p></li>
+      <li><p>Attendance is required at meetings to maintain membership and privileges.</p></li>
+      <li><p>All members must participate in 3 of our projects, activities, and fundraisers.</p></li>
+      <li><p>For the NAHS officers, attendance is also required at any executive meetings called outside of the regularly scheduled meetings.</p></li>
+      <li><p>To graduate with NAHS honors and earn the right to wear a cord, pin, or tassel at graduation, a member must remain in good standing until the graduation date.</p></li>
+      <li><p>Members must retain a C average in an honors or AP class or B in a regular class.</p></li>
+      <li><p>Members must retain a C average in an honors or AP class or B in a regular class.</p></li>
+      <li><p>Members may remain active during such time when there are no visual arts on their class schedules as long as they fulfill all of the other requirements each year.</p></li>
+      <li><p>Officer selection will be held once a year (At the end of the year, we will have a meeting for electing officers for next year)</p></li>
+      <li><p>Application must be submitted before it is due in order to have a chance to be part of the NAHS club</p></li>
+      <li><p>If a member does not maintain one or multiple requirements throughout the year, the member cannot remain in the club. If you wish to notify the club the reason for not maintaining the requirement, you must fill out an Appeals Form and notify the advisor.</p></li>
+    </ol>
+    <h2>NAHS Constitution</h2>
+    <h3>Article I: Name and Purpose</h3>
+    <p>Section 1. The name of this chapter shall be the LAHS Chapter of the National Art Honor Society.</p>
+    <p>Section 2. The purpose of the chapter shall be to: 
+      
+      A. Inspire and recognize those students who have shown outstanding ability in art; 
+      
+      B. Foster excellence and a dedicated spirit to the pursuit of art; 
+      
+      C. Further creative abilities and talents of the Society’s members, as well as the school’s entire student art enrollment; 
+      
+      D. Aid members in working toward the attainment of their highest potential in an art area;
+      
+      E. Bring art to the attention of the school and community; 
+      
+      F. Increase an awareness of art in relation to other areas of the school curriculum 
+      
+      G. Further aesthetic awareness in all aspects of the school’s total program. 
+    </p>
+    <h3>Article II: Local Chapters</h3>
+    <p>Section 1. Public, charter, private and virtual schools, home school networks, and community based non-profit art organizations are eligible to apply for charter through the National Art Education Association (NAEA). </p>
+    <h3>Article III: Membership</h3>
+    <p>
+Section 1. Membership in this chapter shall be based upon art scholarship, service, and character. 
+
+Section 2. To be eligible for selection to membership in this chapter, the candidate must be a high school student enrolled in grade 9, 10, 11 or 12 in this school, and have completed a period equivalent to one semester of art in this school. 
+
+Section 3. An eligible candidate shall have a minimum art scholarship average of B, or this school’s equivalent. This is the national minimum standard of achievement required for eligibility and shall remain fixed. 
+
+Section 4. Members may remain active during such time when there is no art on their class schedule. 
+
+Section 5. Students enrolled in homeschool on a full-time basis and who meet all other eligibility requirements may be considered for NAHS membership. 
+    </p>
+    <h3>Article IV: Selection of Members</h3>
+    <p>
+Section 1. The selection of members to this chapter of the National Art Honor Society shall be by recommendation from the art faculty of this school who are members in good standing of the National Art Education Association (NAEA). Criteria used to determine membership selection beyond faculty recommendation shall be set forth in this chapter’s bylaws. 
+
+Section 2. Any member who falls below the standards that are the basis for selection shall be promptly warned. If during the next regular marking period of the school, the member fails to meet the standards used as basis for selection to the Society, the case shall be passed upon by the chapter council. 
+
+Section 3. When a member is dismissed, he/she must be notified and the membership card returned to the Society sponsor. 
+
+Section 4. An active member of the National Art Honor Society who transfers from this school will be given a letter indicating the status of his/her membership, signed by the sponsor and principal. 
+
+Section 5. An active member of the National Art Honor Society who transfers to this school will be accepted for membership in this chapter. This transfer member must maintain the membership requirements for this chapter in order to retain membership. 
+
+Section 6. NAEA does not participate in matters of member non-selection or dismissal. 
+    </p>
+    <h3>Article V: Officers</h3>
+    <p>
+Section 1. The officers of this chapter shall be president, vice president, secretary, and treasurer (these could be combined), historian/parliamentarian, chair for awards and ceremonies, chair for public relations, and chair for activities. 
+
+Section 2. A majority of votes cast shall be necessary to elect any officer of this chapter. 
+
+Section 3. It shall be the duty of the president to preside at the meetings of this chapter. 
+
+Section 4. The vice president shall preside in the absence of the president, and serve as the chapter’s program chair.
+
+Section 5. The chapter secretary shall keep the minutes of the meetings, a record of all business, and all records on file. 
+
+Section 6. The treasurer shall receive and disburse all funds of the chapter and keep an accurate account of receipts and disbursements in accordance with school regulations. 
+
+Section 7. A chapter council shall consist of the chapter president, vice president, secretary, treasurer, and the chapter sponsor. 
+    </p>
+    <h3>Article VI: Supervision</h3>
+    <p>
+Section 1. An art faculty member [sponsor] shall be designated to supervise and provide guidance for this chapter on a daily basis. 
+
+Section 2. The sponsor shall be a member of the art faculty employed by the school. 
+
+Section 3. The post of sponsor shall be voluntary. 
+
+Section 4. The sponsors must be National Art Education Association members in good standing. Section 5. The activities of this chapter will be subject to the approval of the sponsor and the principal. The principal shall have the final right to approve all activities and decisions of this chapter. 
+    </p>
+    <h3>Article VII: Meetings</h3>
+    <p>
+Section 1. The regular meetings of this chapter shall be held during the school year on days designated by the chapter council and approved by the principal. 
+
+Section 2. The officers of the chapter shall be elected at the last regular meeting of the year. 
+
+Section 3. Special meetings, approved by the chapter council, may be called by the president. 
+
+Section 4. All meetings shall be open meetings and shall be held under the supervision of the sponsor. 
+
+Section 5. This chapter shall conduct its meetings according to Robert’s Rules of Order, Newly Revised, in all points not expressly provided for in the constitution of this chapter. 
+    </p>
+    <h3>Article VIII: Membership Fees</h3>
+    <p>
+Section 1. A yearly NAHS membership fee of $5 per student shall be forwarded to the National Art Honor Society at the NAEA National Office. 
+
+Section 2. A yearly school sponsorship fee is required for each NAHS chapter and entitles the chapter sponsor to all the rights and privileges of one active NAEA membership including all NAEA member and NAHS chapter benefits. The school sponsorship fee is waived for sponsors who are already active NAEA members. Co-sponsors must be active NAEA members. Section 3. School sponsorship fees and student membership may be paid by the individual or school, community group/agency. Fees and registration forms must be submitted to NAEA by the chapter sponsor. 
+
+Section 4. School sponsorship and student membership fees are nonrefundable and nontransferable. 
+
+Section 5. Local chapters may establish and collect annual dues at their discretion for local operating expenses. 
+    </p>
   </section>
 </Content>
+
+<style>
+  ol, p {
+    font-size: 1.4rem;
+    line-height: 1;
+  }
+
+  p {
+    white-space: pre-wrap;
+  }
+</style>
