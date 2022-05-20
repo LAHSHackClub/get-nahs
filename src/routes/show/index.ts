cb726cacc210fb9bb54a270098d26d7dcb48f7fb
SDBagel <sdbagel@gmail.com>
2022-05-20 10:49:41 -0700

    feat: Primary flag for redirect art show

--- src/routes/show/index.ts
+++ src/routes/show/index.ts
@@ -2,7 +2,7 @@
 export async function get({ params }) {
   const meta = await fetch(`https://db.lahs.club/cache/761269d6997543f5a1b86c0bd17e9ef3.json`)
     .then((res: any) => res.json()) as any[];
-  const selectedShow = meta[meta.length-1];
+  const selectedShow = meta.find((show: any) => show.Primary);
  
   return {
     status: 302,
